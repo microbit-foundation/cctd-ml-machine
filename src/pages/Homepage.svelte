10bd24de574b46bf46e95287d6512d5e0135ccc1
Karl-Emil Kjaer Bilstrup <au524252@uni.au.dk>
2023-03-13 14:03:32 +0100

    Added intro video to frontpage of DR version

--- src/pages/Homepage.svelte
+++ src/pages/Homepage.svelte
@@ -24,9 +24,6 @@
 							<div>
 								<ImageSkeleton alt="microbit guide" height={114} src="imgs/microbit_guide.svg" width={200} />
 							</div>
-							<p class="text-center">
-								{$t("content.index.v1")}
-							</p>
 						</div>
 						<p class="absolute text-center w-60px right-0px top-30px">{$t("content.index.recordButtonDescription")}</p>
 					</div>
@@ -37,26 +34,15 @@
 			</div>
 
 			<div
-				class="border-gray-200 border border-solid bg-white rounded-lg p-6 pb-2"
+				class="border-gray-200 border border-solid bg-white rounded-lg p-6 pb-2 flex flex-col"
 			>
 				<p class="text-lg mb-2 font-semibold">
-					{$t("content.index.setupHeading")}
+					{$t("content.index.videoHeading")}
 				</p>
-				<p>{$t("content.index.setupBody1")}</p>
-				<div class="a flex justify-around gap-1 mt-6 mb-6">
-					<div class="flex-col">
-						<ImageSkeleton alt="microbit version 1" height={160} src="imgs/microbitV1.svg" width={200} />
-						<p class="text-center">
-							{$t("content.index.v1")}
-						</p>
-					</div>
-					<div class="flex-col">
-						<ImageSkeleton alt="microbit version 2" height={160} src="imgs/microbitV2.svg" width={200} />
-						<p class="text-center">
-							{$t("content.index.v2")}
-						</p>
-					</div>
-				</div>
+				<!-- svelte-ignore a11y-media-has-caption -->
+				<video class="h-[300px] self-center" controls width=450 poster="imgs/data-trainer-thumpnail.png">
+					<source src="https://mlmachine0video0storage.blob.core.windows.net/ml-machine-videos/Ultrabit_01_Introvideo_Datatræneren.mp4" type="video/mp4">
+				</video>
 			</div>
 		</div>
 	</div>
@@ -69,9 +55,5 @@
         grid-template-areas:
             "a b"
             "a c";
-    }
-
-    .a {
-        grid-area: a;
     }
 </style>

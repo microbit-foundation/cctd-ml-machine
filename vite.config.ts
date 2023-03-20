c71277d1177025cc01b81adf4d7ad9da7f190347
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-03-20 15:57:11 +0100

    Changed the production tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import WindiCSS from 'vite-plugin-windicss'
+import {sveltePreprocess} from "svelte-preprocess/dist/autoProcess";
 
-// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-    plugins: [svelte(), WindiCSS()],
+    plugins: [svelte(
+        {preprocess: sveltePreprocess({typescript: true})}
+    ), WindiCSS()],
+    build: {
+        rollupOptions: {
+            input: "index.html"
+        }
+    }
 })

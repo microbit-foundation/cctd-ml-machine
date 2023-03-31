cb68a1820dc25466a87260e773011aa4638b5889
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-03-31 12:56:42 +0200

    Added windi.config.js for custom theme colors

---
+++ windi.config.js
@@ -0,0 +1,10 @@
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: "#2B5EA7",
+                secondary: "#2CCAC0",
+            },
+        },
+    },
+}

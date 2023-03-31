d903bc8886d11dec6bfe1d5079fee8cdef17f58d
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-03-31 13:29:15 +0200

    Added some needed colors

--- windi.config.js
+++ windi.config.js
@@ -6,6 +6,8 @@ export default {
                 primarytext: "#000000",
                 secondary: "#2CCAC0",
                 secondarytext: "#ffffff",
+                info: "#98A2B3",
+                warning:"#FF7777"
             },
         },
     },

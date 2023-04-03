6241f0fdfb092db613f78e061184d01676f7613f
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-04-03 16:15:54 +0200

    Checked and tweaked the cookie banner for styling inconsistencies

--- src/components/cookie-bannner/CookieBanner.svelte
+++ src/components/cookie-bannner/CookieBanner.svelte
@@ -44,16 +44,16 @@
 		<CookieDescriptionOverlay />
 	</StandardDialog>
 	<div out:fly class="fixed bottom-0 w-full bg-white">
-		<select class="absolute bg-white right-5 top-5 text-black ml-2 mr-2 pl-2 pr-2" bind:value={$locale}>
+		<select class="absolute bg-white right-5 top-5 text-primarytext ml-2 mr-2 pl-2 pr-2" bind:value={$locale}>
 			{#each locales as l}
 				<option value={l}>{l}</option>
 			{/each}
 		</select>
 		<div class="ml-auto mr-auto mt-5">
-			<p class="text-center font-bold text-red-400 text-2xl ml-auto mr-auto">{$t("cookies.banner.title")}</p>
+			<p class="text-center font-bold text-warning text-2xl ml-auto mr-auto">{$t("cookies.banner.title")}</p>
 		</div>
 		<div class="flex w-full mb-5">
-			<div class="flex w-8/10 flex-col">
+			<div class="flex w-3/5 flex-col">
 
 				<div class="mr-20 ml-20 mt-auto mb-auto">
 					<p class="font-bold text-xl mb-2">
@@ -69,7 +69,7 @@
 					<p>
 						{$t("cookies.banner.text.readMore")}
 						<a href="/"
-							class="text-[#63BFC2] font-bold"
+							class="text-secondary font-bold"
 							on:click={(e) => {
 								// Fix this. Why does ts consider this 'any'?
 								// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -83,17 +83,13 @@
 					<div class="mb-2"></div>
 				</div>
 			</div>
-			<div class="flex w-1/4 flex-row mr-4">
-				<div class="flex-1 flex-col m-auto pl-4 pr-4">
-					<StandardButton onClick={acceptClicked}
-					>{$t("cookies.banner.buttons.accept")}</StandardButton>
-				</div>
-				<div class="flex-1 flex-col m-auto pl-4 pr-4">
-					<StandardButton 
+			<div class="flex w-2/5 flex-row mr-4 justify-center">
+					<StandardButton onClick={acceptClicked}>{$t("cookies.banner.buttons.accept")}</StandardButton>
+				<div class="mr-4"></div>
+					<StandardButton
 						color="warning"
 						onClick={rejectClicked}
 					>{$t("cookies.banner.buttons.reject")}</StandardButton>
-				</div>
 			</div>
 		</div>
 	</div>

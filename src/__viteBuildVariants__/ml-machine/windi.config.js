193eb95a9d8289d6caee850afdf3f974cccd6f3d
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-05-01 14:09:32 +0200

    Added config files for ML-Machine and default

---
+++ src/__viteBuildVariants__/ml-machine/windi.config.js
@@ -0,0 +1,27 @@
+// Colors for ML-Machine
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2B5EA7',
+        primarytext: '#000000',
+        secondary: '#2CCAC0',
+        secondarytext: '#FFFFFF',
+        info: '#98A2B3',
+        backgrounddark: '#F5F5F5',
+        backgroundlight: '#ffffff',
+        infolight: '#93c5fd',
+        warning: '#FF7777',
+        disabled: '#8892A3',
+        primaryborder: '#E5E7EB',
+        infobglight: '#E7E5E4',
+        infobgdark: '#57534E',
+        infoiconlight: '#FFFFFF7F',
+        infoicondark: '#787878',
+        infotextlight: '#ffffff',
+        infotextdark: '#787878',
+      },
+    },
+  },
+};

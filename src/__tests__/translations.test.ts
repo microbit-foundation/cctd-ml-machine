08a13fbc9cb1bc3829db0663fc73fc894fa95197
Jon Wissing <jon.wissing@alexandra.dk>
2023-06-28 16:39:34 +0200

    Fix failing test due to a ts-ignore being moved by auto formatting. Remove ts-ignore and add a type assertion instead

--- src/__tests__/translations.test.ts
+++ src/__tests__/translations.test.ts
@@ -13,8 +13,7 @@ describe('Translation tests', () => {
   test('Translations are the same', () => {
     const danishTranslationKeys = Object.getOwnPropertyNames(translations.da);
     for (let i = 0; i < danishTranslationKeys.length; i++) {
-      const danishKey = danishTranslationKeys[i];
-      // @ts-ignore
+      const danishKey = danishTranslationKeys[i] as keyof typeof translations.en;
       expect(
         translations.en[danishKey],
         'Something not the same -> ' + danishKey,

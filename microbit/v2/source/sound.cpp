15817521a35d9b9a4047ed65bdcdad155e9369ba
r59q <andreashenriksen@yahoo.dk>
2023-08-13 16:42:16 +0200

    Added identifiers and update identifier test

--- microbit/v2/source/sound.cpp
+++ microbit/v2/source/sound.cpp
@@ -1,3 +1,8 @@
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 #include "MicroBit.h"
 #include "utilities.h"
 #include "sounds.h"
--- microbit/v2/source/sounds.h
+++ microbit/v2/source/sounds.h
@@ -1,3 +1,8 @@
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 #include "utilities.h"
 #ifndef SOUNDS_H_INCLUDED
 #define SOUNDS_H_INCLUDED

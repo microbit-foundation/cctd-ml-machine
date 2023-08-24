cc54b764fad2de9a1d3ce98175c06e2149bb02e2
r59q <andreashenriksen@yahoo.dk>
2023-08-25 00:14:33 +0200

    Added missing license identifier

--- src/components/output/OutputGesture.svelte
+++ src/components/output/OutputGesture.svelte
@@ -1,3 +1,8 @@
+<!--
+  (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ 
+  SPDX-License-Identifier: MIT
+ -->
 <script lang="ts">
   import { GestureData } from '../../script/stores/mlStore';
   import OutputGestureStack from './OutputGestureStack.svelte';

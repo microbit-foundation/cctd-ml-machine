450e3dca7517d09b5d8f2657fa4b1a1170413deb
r59q <andreashenriksen@yahoo.dk>
2023-08-24 21:51:15 +0200

    Added missing license identifier

--- src/pages/model/ModelPage.svelte
+++ src/pages/model/ModelPage.svelte
@@ -1,3 +1,9 @@
+<!--
+  (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ 
+  SPDX-License-Identifier: MIT
+ -->
+
 <script lang="ts">
   import CookieManager from '../../script/CookieManager';
   import { state } from '../../script/stores/uiStore';

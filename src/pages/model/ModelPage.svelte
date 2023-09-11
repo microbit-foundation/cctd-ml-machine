4508f9ea37c152a9f829a20195235a8a6b52af0a
r59q <andreashenriksen@yahoo.dk>
2023-09-11 21:38:37 +0200

    Added makecode link to controlbar

--- src/pages/model/ModelPage.svelte
+++ src/pages/model/ModelPage.svelte
@@ -5,20 +5,33 @@
  -->
 
 <script lang="ts">
+  import StandardButton from '../../components/StandardButton.svelte';
   import ControlBar from '../../components/control-bar/ControlBar.svelte';
+  import ExpandableControlBarMenu from '../../components/control-bar/control-bar-items/ExpandableControlBarMenu.svelte';
   import { ModelView, state } from '../../script/stores/uiStore';
   import ModelPageStackView from './ModelPageStackView.svelte';
   import ModelPageTileView from './ModelPageTileView.svelte';
-  import windiConfig from '../../../windi.config';
 
   const switchView = () => {
     $state.modelView =
       $state.modelView == ModelView.STACK ? ModelView.TILE : ModelView.STACK;
   };
+
+  const openMakeCodeInNewTab = () => {
+    window.open(
+      'https://makecode.microbit.org/#pub:04730-01245-34848-93416',
+      '_blank', // <- This is what makes it open in a new window.
+    );
+  };
 </script>
 
 <div>
-  <ControlBar />
+  <ControlBar>
+    <ExpandableControlBarMenu>
+      <StandardButton small outlined onClick={openMakeCodeInNewTab}
+        >MakeCode</StandardButton>
+    </ExpandableControlBarMenu>
+  </ControlBar>
 </div>
 <div class="pt-4 pl-3">
   {#if $state.modelView == ModelView.TILE}

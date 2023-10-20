997f55eee485d3138033bbc95e23dc3f4c6d53a9
Marisa <marisa.jenkins@arm.com>
2023-10-20 11:51:28 +0100

    Introduction video modal with temporary text

--- src/components/IntroductionVideo.svelte
+++ src/components/IntroductionVideo.svelte
@@ -16,28 +16,28 @@
 </script>
 
 <StandardDialog {isOpen} onClose={() => (isOpen = false)}>
-  <div class="w-100">
-    <p class="text-lg mb-2 font-semibold">
+  <div class="w-150">
+    <p class="text-2xl mb-2 font-semibold">
       {$t('content.index.videoHeading')}
     </p>
     <div class="flex items-center justify-center">
       <!-- We have selected the video's loading progress as the loading criterion -->
       <video
         on:canplaythrough={() => ($state.isLoading = false)}
-        class="h-[300px]"
+        class="w-full"
         controls
-        width="450"
+        width="550"
         poster="imgs/data-trainer-thumpnail.png">
         <source
           src="https://datatraener0dr0media-euno.streaming.media.azure.net/18233c69-2bc3-4b1b-9e2d-249e37b56307/Ultrabit_01_Introvideo_Datatræneren.mp4"
           type="video/mp4" />
       </video>
     </div>
-    <p>TEMPORARY TEXT</p>
     <!-- MARISA: note -> change text for button to 'get started' -->
-    <div class="">
+    <p class="pt-5">TEMPORARY TEXT</p>
+    <div class="ml-110">
       <StandardButton onClick={() => isOpen = false}
-        >{$t('popup.connectMB.bluetooth.connect')}</StandardButton>
+        >TEMPORARY TEXT</StandardButton>
     </div>
   </div>
 </StandardDialog>

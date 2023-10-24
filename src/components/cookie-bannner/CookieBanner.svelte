ddc892f87c08e8ab9a8c59e070a82be4d9375090
Marisa <marisa.jenkins@arm.com>
2023-10-24 16:44:55 +0100

    Chaning cookie banner design to align with the Micro:bit foundation one

--- src/components/cookie-bannner/CookieBanner.svelte
+++ src/components/cookie-bannner/CookieBanner.svelte
@@ -10,11 +10,10 @@
   import { t, locale, locales } from '../../i18n';
   import StandardDialog from '../dialogs/StandardDialog.svelte';
   import { onMount } from 'svelte';
-  import BaseDialog from '../dialogs/BaseDialog.svelte';
-  import CookieDescriptionOverlay from './CookieDescriptionOverlay.svelte';
   import StandardButton from '../StandardButton.svelte';
+    import CookieDescriptionOverlay from './CookieDescriptionOverlay.svelte';
 
-  let isCookieDialogOpen: boolean;
+  let isCookieDialogOpen = true;
   let isDescriptionDialogOpen = false;
 
   const acceptClicked = () => {
@@ -43,58 +42,52 @@
   });
 </script>
 
-<BaseDialog
-  isOpen={isCookieDialogOpen}
-  onClose={() => {
-    return;
-  }}>
+<StandardDialog isOpen={isCookieDialogOpen} onClose={() => (isCookieDialogOpen = false)}>
   <StandardDialog
     isOpen={isDescriptionDialogOpen}
     onClose={() => (isDescriptionDialogOpen = false)}>
     <CookieDescriptionOverlay />
   </StandardDialog>
-  <div out:fly class="fixed bottom-0 w-full bg-white">
+  <div out:fly class="w-200">
     <select
-      class="absolute bg-white right-5 top-5 text-primarytext ml-2 mr-2 pl-2 pr-2"
-      bind:value={$locale}>
-      {#each locales as l}
-        <option value={l}>{l}</option>
-      {/each}
-    </select>
+        class="absolute bg-white right-5 top-5 text-primarytext mr-5 pl-2 pr-2"
+        bind:value={$locale}>
+        {#each locales as l}
+          <option value={l}>{l}</option>
+        {/each}
+      </select>
     <div class="ml-auto mr-auto mt-5">
-      <p class="text-center font-bold text-warning text-2xl ml-auto mr-auto">
+      <p class="text-left p-b-10 font-bold text-warning text-3xl ml-auto mr-auto">
         {$t('cookies.banner.title')}
       </p>
     </div>
-    <div class="flex w-full mb-5">
-      <div class="flex w-3/5 flex-col">
-        <div class="mr-20 ml-20 mt-auto mb-auto">
-          <p class="font-bold text-xl mb-2">
-            {$t('cookies.banner.subtitle')}
-          </p>
-          <p>
-            {$t('cookies.banner.text.pleaseHelp')}
-          </p>
-          <p>
-            {$t('cookies.banner.text.description')}
-          </p>
-          <div class="mb-2" />
-          <p>
-            {$t('cookies.banner.text.readMore')}
-            <a href="/" class="text-secondary font-bold" on:click={readMoreClicked}>
-              {$t('cookies.banner.text.readMore.here')}
-            </a>
-          </p>
-          <div class="mb-2" />
-        </div>
-      </div>
-      <div class="flex w-2/5 flex-row mr-4 justify-center">
-        <StandardButton onClick={acceptClicked}
-          >{$t('cookies.banner.buttons.accept')}</StandardButton>
-        <div class="mr-4" />
-        <StandardButton color="warning" onClick={rejectClicked}
-          >{$t('cookies.banner.buttons.reject')}</StandardButton>
+    <div class="flex w-full flex-col">
+      <div class="mr-20 ml-20 mt-auto mb-auto">
+        <p class="font-bold text-xl mb-2">
+          {$t('cookies.banner.subtitle')}
+        </p>
+        <p>
+          {$t('cookies.banner.text.pleaseHelp')}
+        </p>
+        <p>
+          {$t('cookies.banner.text.description')}
+        </p>
+        <div class="mb-2" />
+        <p>
+          {$t('cookies.banner.text.readMore')}
+          <a href="/" class="text-secondary font-bold" on:click={readMoreClicked}>
+            {$t('cookies.banner.text.readMore.here')}
+          </a>
+        </p>
+        <div class="mb-2" />
       </div>
     </div>
+    <div class="flex mr-4 justify-end">
+      <StandardButton onClick={acceptClicked}
+        >{$t('cookies.banner.buttons.accept')}</StandardButton>
+      <div class="mr-4" />
+      <StandardButton color="warning" onClick={rejectClicked}
+        >{$t('cookies.banner.buttons.reject')}</StandardButton>
+    </div>
   </div>
-</BaseDialog>
+</StandardDialog>

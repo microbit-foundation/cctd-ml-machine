fbf238a3209848871792d1c5163fe484fed611ac
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-10-31 17:48:10 +0100

    License identifiers

--- src/script/ControlledStorage.ts
+++ src/script/ControlledStorage.ts
@@ -1,3 +1,8 @@
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 type StoredValue<T> = {
   version: number;
   value: T;
--- src/script/gestures/GestureRepository.ts
+++ src/script/gestures/GestureRepository.ts
@@ -1,3 +1,8 @@
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 import { PersistantGestureData } from '../stores/Gestures';
 import Gesture from '../stores/Gesture';
 import ControlledStorage from '../ControlledStorage';

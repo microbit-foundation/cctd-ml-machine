a51e2ccda317fe092ade5ac4a97e63eeb552e893
Michael Quested <michael.quested@arm.com>
2023-12-13 16:27:09 +0000

    Disable 3d graph views

--- src/components/bottom/BottomPanel.svelte
+++ src/components/bottom/BottomPanel.svelte
@@ -15,7 +15,10 @@
   import BaseDialog from '../dialogs/BaseDialog.svelte';
   import View3DLive from '../3d-inspector/View3DLive.svelte';
 
+  const live3dViewVisible = false;
+  const live3dViewSize = live3dViewVisible ? 160 : 0;
   let componentWidth: number;
+  let isLive3DOpen = false;
 
   const inputDisconnectButtonClicked = () => {
     Microbits.expelInputAndOutput();
@@ -24,8 +27,6 @@
   const outputDisconnectButtonClicked = () => {
     Microbits.expelOutput();
   };
-
-  let isLive3DOpen = false;
 </script>
 
 <div
@@ -36,7 +37,7 @@
     <!-- Input microbit is assigned -->
     <div class="relative w-full h-full bg-white">
       <div class="absolute w-full h-full">
-        <LiveGraph width={componentWidth - 160} />
+        <LiveGraph width={componentWidth - live3dViewSize} />
       </div>
       {#if !$state.isInputReady}
         <!-- Input is not ready, but is assigned (Must be either reconnecting or have lost connection entirely) -->
@@ -62,13 +63,13 @@
       <div
         class="absolute right-0 cursor-pointer hover:bg-secondary hover:bg-opacity-10 transition"
         on:click={() => (isLive3DOpen = true)}>
-        <View3DLive width={160} height={160} freeze={isLive3DOpen} />
+        <View3DLive width={live3dViewSize} height={live3dViewSize} freeze={isLive3DOpen} />
       </div>
       <BaseDialog isOpen={isLive3DOpen} onClose={() => (isLive3DOpen = false)}>
         <!-- hardcoded margin-left matches the size of the sidebar -->
         <div
           class="ml-75 border-gray-200 overflow-hidden border border-solid relative bg-white rounded-1 shadow-dark-400 shadow-md flex justify-center"
-          style="height: calc(100vh - 160px); width: calc(100vh - 160px);">
+          style="height: calc(100vh - {live3dViewSize}px); width: calc(100vh - {live3dViewSize}px);">
           <div class="-mt-5 w-full h-full justify-center align-middle flex items-center">
             <View3DLive width={600} height={600} smoothing />
           </div>

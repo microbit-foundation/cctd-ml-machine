df00979114505de51940d286826236f04a760a10
Marisa <marisa.jenkins@arm.com>
2023-12-14 09:58:36 +0000

    switching order of recognition bar and certainty

--- src/components/output/OutputGestureStack.svelte
+++ src/components/output/OutputGestureStack.svelte
@@ -197,7 +197,7 @@
 </script>
 
 <main class="mb-4 items-center flex flex-row space-x-10 pl-20">
-  <!-- NAMES AND CONFIDENCE METER -->
+  <!-- ACTION TITLE -->
   <GestureTilePart>
     <div class="items-center flex p-2 w-100">
       <div
@@ -209,7 +209,35 @@
   </GestureTilePart>
 
   <GestureTilePart>
-  <div class="h-31" />
+    
+    <!-- METER -->
+    <div class="w-4 h-25 relative">
+      <div
+      class="w-4 h-full absolute rounded border border-solid border-gray-400 overflow-hidden">
+      <div
+      class="absolute w-5 {wasTriggered ? 'bg-primary' : 'bg-info'} z-index: -10"
+      style="height: {meterHeightPct}px; margin-top: {100 - meterHeightPct}px;" />
+      <div
+      class="absolute w-5 bg-primary"
+      style="height: 1px; margin-top: {6.5 - 0.068 * sliderValue}rem;" />
+      <div class="absolute">
+        {#each [75, 50, 25] as line}
+        <div class="w-5 bg-gray-300 mt-6" style="height: 1px;">
+          <p class="absolute text-xs" style="margin-top: -8px; margin-left: 18px;">
+            {line}%
+          </p>
+        </div>
+        {/each}
+      </div>
+      <div />
+    </div>
+  </div>
+  <div class="relative self-start">
+    <Information
+    titleText="{$t('content.model.classification.helpHeading')}"
+    bodyText={$t('content.model.classification.helpBody')}
+    isLightTheme={false} />
+  </div>
   <input
     class="h-25 rotate-90 accent-primary"
     type="range"
@@ -218,35 +246,6 @@
     max="90"
     id=""
     bind:value={sliderValue} />
-
-  <!-- METER -->
-  <div class="w-4 h-25 relative">
-    <div
-      class="w-4 h-full absolute rounded border border-solid border-gray-400 overflow-hidden">
-      <div
-        class="absolute w-5 {wasTriggered ? 'bg-primary' : 'bg-info'} z-index: -10"
-        style="height: {meterHeightPct}px; margin-top: {100 - meterHeightPct}px;" />
-      <div
-        class="absolute w-5 bg-primary"
-        style="height: 1px; margin-top: {6.5 - 0.068 * sliderValue}rem;" />
-      <div class="absolute">
-        {#each [75, 50, 25] as line}
-          <div class="w-5 bg-gray-300 mt-6" style="height: 1px;">
-            <p class="absolute text-xs" style="margin-top: -8px; margin-left: 18px;">
-              {line}%
-            </p>
-          </div>
-        {/each}
-      </div>
-      <div />
-    </div>
-  </div>
-  <div class="relative self-start">
-    <Information
-      titleText="{$t('content.model.classification.helpHeading')}"
-      bodyText={$t('content.model.classification.helpBody')}
-      isLightTheme={false} />
-  </div>
   </GestureTilePart>
 
   {#if enableOutputGestures}

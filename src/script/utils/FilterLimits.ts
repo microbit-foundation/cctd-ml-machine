f466845e60813a2c127424d679e56b707534eac4
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-12-20 22:02:52 +0100

    Fixed test cases

--- src/script/utils/FilterLimits.ts
+++ src/script/utils/FilterLimits.ts
@@ -1,3 +1,8 @@
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 import Filter from '../domain/Filter';
 import { FilterType } from '../domain/FilterTypes';
 

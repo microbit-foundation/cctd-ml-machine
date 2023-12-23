876d6922edcf5e29e67b1c99012a8c030933eda0
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-12-23 11:28:36 +0100

    Added license identifier

--- src/components/Range.svelte
+++ src/components/Range.svelte
@@ -1,4 +1,9 @@
-<style>
+<!--
+  (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ 
+  SPDX-License-Identifier: MIT
+ -->
+ <style>
   .range {
     position: relative;
     flex: 1;

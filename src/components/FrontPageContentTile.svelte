96f63f22459abe7a30d76ec7f493072cebedf757
Grace <145345672+microbit-grace@users.noreply.github.com>
2024-01-03 17:25:50 +0000

    Tweak homepage card shadow (#101)

--- src/components/FrontPageContentTile.svelte
+++ src/components/FrontPageContentTile.svelte
@@ -5,6 +5,6 @@
  -->
 
 <div
-  class="flex flex-col flex-wrap items-center border-gray-200 border border-solid bg-white rounded-3xl p-6 shadow-xl max-w-88">
+  class="flex flex-col flex-wrap items-center border-gray-200 border border-solid bg-white rounded-3xl p-6 shadow-md max-w-88">
   <slot />
 </div>

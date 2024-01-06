75b0e682d7349c71a89c1c8fb15baf50d11faebd
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2024-01-06 16:53:33 +0100

    Added gestures test and a couple of gestures

---
+++ src/__tests__/gestures.test.ts
@@ -0,0 +1,25 @@
+/**
+ * @vitest-environment jsdom
+ */
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { gestures } from "../script/stores/Stores";
+
+describe('Tests of Gestures', () => {
+  test('Creating gesture does not throw', () => {
+    expect(() => {
+        gestures.createGesture("test");
+    }).not.toThrow();
+  });
+
+  test('Can get gesture after creation', () => {
+    const gestureName = "test1234"
+    const gesture = gestures.createGesture(gestureName);
+    const fetchedGesture = gestures.getGesture(gesture.getId());
+    expect(gestureName).toBe(fetchedGesture.getName());
+  })
+});

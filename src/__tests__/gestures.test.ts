d6188aa4d212cde26a5ecde522574cda968d7c97
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2024-01-06 17:26:12 +0100

    Updated gestures test

--- src/__tests__/gestures.test.ts
+++ src/__tests__/gestures.test.ts
@@ -10,16 +10,35 @@
 import { gestures } from '../script/stores/Stores';
 
 describe('Tests of Gestures', () => {
+  beforeEach(() => {
+    gestures.clearGestures();
+  });
   test('Creating gesture does not throw', () => {
     expect(() => {
       gestures.createGesture('test');
     }).not.toThrow();
   });
 
+  test('Creating 2 gestures makes total number of gesture 2', () => {
+    gestures.createGesture('Gesture1');
+    gestures.createGesture('Gesture2');
+
+    expect(gestures.getGestures().length).toBe(2);
+  });
+
   test('Can get gesture after creation', () => {
     const gestureName = 'test1234';
     const gesture = gestures.createGesture(gestureName);
     const fetchedGesture = gestures.getGesture(gesture.getId());
-    expect(gestureName).toBe(fetchedGesture.getName());
+    expect(fetchedGesture.getName()).toBe(gestureName);
+  });
+
+  test('Clearing gestures clears gestures', () => {
+    gestures.createGesture('gestureName');
+    gestures.createGesture('gestureName');
+
+    gestures.clearGestures();
+
+    expect(gestures.getGestures().length).toBe(0);
   });
 });

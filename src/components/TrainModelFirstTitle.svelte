a7bb63bed9e5f5a675f95d2fc9089f320fc27135
Grace <145345672+microbit-grace@users.noreply.github.com>
2024-01-11 14:12:29 +0000

    Add insufficient data and ready to train states to test model page (#135)

--- src/components/TrainModelFirstTitle.svelte
+++ src/components/TrainModelFirstTitle.svelte
@@ -10,6 +10,9 @@
   import StandardButton from './StandardButton.svelte';
   import TrainingButton from '../pages/training/TrainingButton.svelte';
   import { Paths, navigate } from '../router/paths';
+  import { hasSufficientData } from '../script/stores/uiStore';
+
+  $: sufficientData = hasSufficientData();
 </script>
 
 <div class="flex flex-col items-center bg-backgrounddark pb-10">
@@ -17,16 +20,23 @@
   <h1 class="text-2xl font-bold mb-3">
     {$t('content.model.trainModelFirstHeading')}
   </h1>
-  <p class="mb-5">
-    {$t('content.model.trainModelBody1')}
-  </p>
-  <p class="mb-10">
-    {$t('content.model.trainModelBody2')}
-  </p>
-  <div class="flex gap-5">
-    <StandardButton onClick={() => navigate(Paths.DATA)}>
-      {$t('content.model.addData')}
-    </StandardButton>
-    <TrainingButton type="secondary" onClick={() => navigate(Paths.TRAINING)} />
+  <div class="space-y-5">
+    <p>
+      {$t('content.model.trainModelBody')}
+    </p>
+    {#if !sufficientData}
+      <p>
+        {$t('content.model.notEnoughDataInfoBody')}
+      </p>
+    {/if}
+  </div>
+  <div class="mt-10 flex gap-5">
+    {#if !sufficientData}
+      <StandardButton type="primary" onClick={() => navigate(Paths.DATA)}>
+        {$t('content.model.addData')}
+      </StandardButton>
+    {:else}
+      <TrainingButton type="primary" onClick={() => navigate(Paths.TRAINING)} />
+    {/if}
   </div>
 </div>

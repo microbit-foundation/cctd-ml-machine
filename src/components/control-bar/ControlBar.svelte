6785a4c8b3797490180e308430f2b14981019a9e
Robert Knight <95928279+microbit-robert@users.noreply.github.com>
2024-01-23 13:45:39 +0000

    Tweak control bar z-index to fix visual bug (#181)

--- src/components/control-bar/ControlBar.svelte
+++ src/components/control-bar/ControlBar.svelte
@@ -5,6 +5,6 @@
  -->
 
 <div
-  class="min-h-16 px-10 flex w-full items-center justify-between bg-secondary sticky top-0 z-4">
+  class="min-h-16 px-10 flex w-full items-center justify-between bg-secondary sticky top-0 z-5">
   <slot />
 </div>

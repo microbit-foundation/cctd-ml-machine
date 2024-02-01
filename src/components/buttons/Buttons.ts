1631621964b3836446a672a37f906f3e8e17be81
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2024-02-01 20:57:38 +0100

    License identifier

--- src/components/buttons/Buttons.ts
+++ src/components/buttons/Buttons.ts
@@ -1,3 +1,8 @@
+/**
+ * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 export type DropdownOption = {
   id: unknown;
   label: string;

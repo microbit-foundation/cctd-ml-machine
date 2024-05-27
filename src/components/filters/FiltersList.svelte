54484b9b3067859890028c7a6ebc90df70e80e1e
r59q <andreashenriksen@yahoo.dk>
2024-05-27 22:24:31 +0200

    Create filters list

---
+++ src/components/filters/FiltersList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import FilterTypes from '../../script/domain/FilterTypes';
+  import FilterListRow from './FilterListRow.svelte';
+
+  const availableFilters = FilterTypes.toIterable();
+</script>
+
+<div class="flex flex-col border-solid border-2 border-secondary rounded-md shadow-md">
+  {#each availableFilters as filterType}
+    <FilterListRow {filterType} />
+  {/each}
+</div>
--- src/pages/training/TrainingPage.svelte
+++ src/pages/training/TrainingPage.svelte
@@ -22,6 +22,7 @@
   import CookieManager from '../../script/CookieManager';
   import { stores } from '../../script/stores/Stores';
   import { appInsights } from '../../appInsights';
+  import FiltersList from '../../components/filters/FiltersList.svelte';
 
   const classifier = stores.getClassifier();
   const gestures = stores.getGestures();
@@ -77,84 +78,89 @@
       {$t('content.trainer.controlbar.filters')}
     </StandardButton>
   </ControlBar>
-  <div class="flex flex-col flex-grow justify-center items-center text-center">
-    {#if isUsingKNNModel}
-      <KnnModelGraph />
-    {/if}
-    {#if !sufficientData}
-      <div class="w-full text-primarytext">
-        <h1 class="w-3/4 text-3xl bold m-auto">
-          {$t('menu.trainer.notEnoughDataHeader1')}
-        </h1>
-        <p class="w-3/5 text-xl m-auto mt-5">
-          {$t('menu.trainer.notEnoughDataInfoBody')}
-        </p>
-      </div>
-    {:else}
-      <div class="w-3/4 text-primarytext">
-        {#if !$model.isTraining}
+  <div class="flex flex-row">
+    <div class="m-2">
+      <FiltersList />
+    </div>
+    <div class="flex flex-col flex-grow justify-center items-center text-center">
+      {#if isUsingKNNModel}
+        <KnnModelGraph />
+      {/if}
+      {#if !sufficientData}
+        <div class="w-full text-primarytext">
+          <h1 class="w-3/4 text-3xl bold m-auto">
+            {$t('menu.trainer.notEnoughDataHeader1')}
+          </h1>
+          <p class="w-3/5 text-xl m-auto mt-5">
+            {$t('menu.trainer.notEnoughDataInfoBody')}
+          </p>
+        </div>
+      {:else}
+        <div class="w-3/4 text-primarytext">
+          {#if !$model.isTraining}
+            {#if $filters.length == 0}
+              <p class="bold text-xl bold mt-10">
+                {$t('menu.trainer.noFilters')}
+              </p>
+            {:else}
+              <div class="w-full pt-5 text-white pb-5">
+                <TrainModelButton
+                  selectedOption={selectedModelOption}
+                  onClick={() => {
+                    resetLoss();
+                    trackModelEvent();
+                  }}
+                  onTrainingIteration={trainingIterationHandler} />
+              </div>
+            {/if}
+          {/if}
+
+          {#if $model.isTrained}
+            <p class="bold text-3xl bold mt-5">
+              {$t('menu.trainer.TrainingFinished')}
+            </p>
+            <p class="bold text-xl bold mt-5">
+              {$t('menu.trainer.TrainingFinished.body')}
+            </p>
+          {/if}
           {#if $filters.length == 0}
             <p class="bold text-xl bold mt-10">
               {$t('menu.trainer.noFilters')}
             </p>
-          {:else}
-            <div class="w-full pt-5 text-white pb-5">
-              <TrainModelButton
-                selectedOption={selectedModelOption}
-                onClick={() => {
-                  resetLoss();
-                  trackModelEvent();
-                }}
-                onTrainingIteration={trainingIterationHandler} />
-            </div>
           {/if}
-        {/if}
-
-        {#if $model.isTrained}
-          <p class="bold text-3xl bold mt-5">
-            {$t('menu.trainer.TrainingFinished')}
-          </p>
-          <p class="bold text-xl bold mt-5">
-            {$t('menu.trainer.TrainingFinished.body')}
-          </p>
-        {/if}
-        {#if $filters.length == 0}
-          <p class="bold text-xl bold mt-10">
-            {$t('menu.trainer.noFilters')}
-          </p>
-        {/if}
-      </div>
-    {/if}
-    {#if !isUsingKNNModel}
-      <div class="mt-10">
-        {#if $loss.length > 0 || $model.isTraining}
-          {#if !CookieManager.hasFeatureFlag('loss-graph')}
-            {#if $model.isTraining}
-              <div
-                class="flex flex-col flex-grow justify-center items-center text-center">
-                <div class="w-3/4 text-primarytext">
-                  <div class="ml-auto mr-auto flex center-items justify-center">
-                    <i
-                      class="fa fa-solid fa-circle-notch text-5xl animate-spin animate-duration-[2s]" />
+        </div>
+      {/if}
+      {#if !isUsingKNNModel}
+        <div class="mt-10">
+          {#if $loss.length > 0 || $model.isTraining}
+            {#if !CookieManager.hasFeatureFlag('loss-graph')}
+              {#if $model.isTraining}
+                <div
+                  class="flex flex-col flex-grow justify-center items-center text-center">
+                  <div class="w-3/4 text-primarytext">
+                    <div class="ml-auto mr-auto flex center-items justify-center">
+                      <i
+                        class="fa fa-solid fa-circle-notch text-5xl animate-spin animate-duration-[2s]" />
+                    </div>
+                    <p class="bold text-3xl bold mt-10">
+                      {$t('menu.trainer.isTrainingModelButton')}
+                    </p>
                   </div>
-                  <p class="bold text-3xl bold mt-10">
-                    {$t('menu.trainer.isTrainingModelButton')}
-                  </p>
                 </div>
-              </div>
+              {/if}
+            {:else}
+              <LossGraph
+                {loss}
+                maxX={StaticConfiguration.layersModelTrainingSettings.noOfEpochs} />
             {/if}
-          {:else}
-            <LossGraph
-              {loss}
-              maxX={StaticConfiguration.layersModelTrainingSettings.noOfEpochs} />
           {/if}
-        {/if}
-      </div>
-    {/if}
-    {#if !$state.isInputConnected}
-      <div class="mt-5">
-        <PleaseConnectFirst />
-      </div>
-    {/if}
+        </div>
+      {/if}
+      {#if !$state.isInputConnected}
+        <div class="mt-5">
+          <PleaseConnectFirst />
+        </div>
+      {/if}
+    </div>
   </div>
 </div>

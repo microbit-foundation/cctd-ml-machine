209ee38b47fcfa9e8649d9617cb345f53fd8ad58
Grace <grace@microbit.org>
2024-07-05 11:54:20 +0100

    Add data action panel UI

--- src/pages/AddDataPage.tsx
+++ src/pages/AddDataPage.tsx
@@ -1,4 +1,23 @@
-import { Grid, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Grid,
+  HStack,
+  Icon,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  VStack,
+} from "@chakra-ui/react";
+import { MdMoreVert } from "react-icons/md";
+import {
+  RiAddLine,
+  RiDeleteBin2Line,
+  RiDownload2Line,
+  RiUpload2Line,
+} from "react-icons/ri";
+import { FormattedMessage, useIntl } from "react-intl";
 import ConnectFirstView from "../components/ConnectFirstView";
 import DefaultPageLayout from "../components/DefaultPageLayout";
 import LiveGraphPanel from "../components/LiveGraphPanel";
@@ -6,6 +25,7 @@ import TabView from "../components/TabView";
 import { addDataConfig } from "../steps-config";
 
 const AddDataPage = () => {
+  const intl = useIntl();
   const noStoredData = true;
   const isInputConnected = false;
   return (
@@ -18,6 +38,46 @@ const AddDataPage = () => {
           <Grid>TODO: Grid layout!</Grid>
         )}
       </VStack>
+      <HStack
+        justifyContent="space-between"
+        px={10}
+        py={2}
+        borderBottomWidth={3}
+        borderTopWidth={3}
+        borderColor="gray.200"
+        alignItems="center"
+      >
+        <Button variant="primary" leftIcon={<RiAddLine />}>
+          <FormattedMessage id="content.data.addAction" />
+        </Button>
+        <HStack gap={2} alignItems="center">
+          <Button>
+            <FormattedMessage id="menu.trainer.trainModelButton" />
+          </Button>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label={intl.formatMessage({
+                id: "content.data.controlbar.button.menu",
+              })}
+              variant="ghost"
+              icon={<Icon as={MdMoreVert} boxSize={10} color="brand.500" />}
+              isRound
+            />
+            <MenuList>
+              <MenuItem icon={<RiUpload2Line />}>
+                <FormattedMessage id="content.data.controlbar.button.uploadData" />
+              </MenuItem>
+              <MenuItem icon={<RiDownload2Line />}>
+                <FormattedMessage id="content.data.controlbar.button.downloadData" />
+              </MenuItem>
+              <MenuItem icon={<RiDeleteBin2Line />}>
+                <FormattedMessage id="content.data.controlbar.button.clearData" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </HStack>
+      </HStack>
       <LiveGraphPanel />
     </DefaultPageLayout>
   );

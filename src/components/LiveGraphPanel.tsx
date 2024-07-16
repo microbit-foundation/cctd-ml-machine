be321137e8d940a7d1eebad4cdf87160df6cd180
Grace <grace@microbit.org>
2024-07-16 11:26:32 +0100

    Use Portal to control live graph panel buttons clickability

--- src/components/LiveGraphPanel.tsx
+++ src/components/LiveGraphPanel.tsx
@@ -1,14 +1,15 @@
-import { Button, HStack, Text } from "@chakra-ui/react";
+import { Button, HStack, Portal, Text } from "@chakra-ui/react";
 import { MdBolt } from "react-icons/md";
 import { FormattedMessage } from "react-intl";
 import InfoToolTip from "./InfoToolTip";
 import LiveGraph from "./LiveGraph";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 import { useConnectionFlow } from "../connections";
 import { ConnEvent } from "../connection-flow";
 
 const LiveGraphPanel = () => {
   const { dispatch } = useConnectionFlow();
+  const parentPortalRef = useRef(null);
 
   // TODO: replace with hook
   const isConnected = false;
@@ -18,34 +19,41 @@ const LiveGraphPanel = () => {
     dispatch(ConnEvent.Start);
   }, [dispatch]);
   return (
-    <HStack position="relative" h={160} width="100%" bgColor="white">
-      <HStack
-        justifyContent="space-between"
-        position="absolute"
-        top={0}
-        left={0}
-        right={0}
-        px={7}
-        py={4}
-        zIndex={1}
-      >
-        <HStack gap={4}>
-          <LiveIndicator />
-          {isConnected ? (
-            <Button variant="primary" size="sm">
-              <FormattedMessage id="footer.disconnectButton" />
-            </Button>
-          ) : (
-            <Button variant="primary" size="sm" onClick={handleConnect}>
-              <FormattedMessage id="footer.connectButton" />
-            </Button>
-          )}
+    <HStack
+      position="relative"
+      h={160}
+      width="100%"
+      bgColor="white"
+      ref={parentPortalRef}
+    >
+      <Portal containerRef={parentPortalRef}>
+        <HStack
+          justifyContent="space-between"
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          px={7}
+          py={4}
+        >
+          <HStack gap={4}>
+            <LiveIndicator />
+            {isConnected ? (
+              <Button variant="primary" size="sm">
+                <FormattedMessage id="footer.disconnectButton" />
+              </Button>
+            ) : (
+              <Button variant="primary" size="sm" onClick={handleConnect}>
+                <FormattedMessage id="footer.connectButton" />
+              </Button>
+            )}
+          </HStack>
+          <InfoToolTip
+            titleId="footer.helpHeader"
+            descriptionId="footer.helpContent"
+          />
         </HStack>
-        <InfoToolTip
-          titleId="footer.helpHeader"
-          descriptionId="footer.helpContent"
-        />
-      </HStack>
+      </Portal>
       <HStack position="absolute" width="100%" height="100%">
         <LiveGraph />
       </HStack>

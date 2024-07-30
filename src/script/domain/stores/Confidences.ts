f33a49b279b5692739dfa2db760be9649260520d
r59q <andreashenriksen@yahoo.dk>
2024-07-30 20:33:58 +0200

    Create confidences store

---
+++ src/script/domain/stores/Confidences.ts
@@ -0,0 +1,36 @@
+import { Readable, Subscriber, Unsubscriber, Writable, get, writable } from "svelte/store";
+import { GestureID } from "./gesture/Gesture";
+
+type GestureConfidenceMap = Map<GestureID, number>;
+
+class Confidences implements Readable<GestureConfidenceMap> {
+    private confidenceStore: Writable<GestureConfidenceMap>;
+
+    constructor() {
+        this.confidenceStore = writable(new Map<GestureID, number>());
+    }
+
+    public subscribe(
+        run: Subscriber<GestureConfidenceMap>,
+        invalidate?: ((value?: GestureConfidenceMap | undefined) => void) | undefined
+    ): Unsubscriber {
+        return this.confidenceStore.subscribe(run, invalidate);
+    }
+
+    public setConfidence(gestureId: GestureID, confidence: number): void {
+        this.confidenceStore.update((map: GestureConfidenceMap) => {
+            map.set(gestureId, confidence);
+            return map;
+        });
+    }
+
+    public getConfidence(gestureId: GestureID): number {
+        const confidence = get(this.confidenceStore).get(gestureId);
+        if (confidence === undefined) {
+            throw new Error(`No confidence value found for gesture with ID ${gestureId}`);
+        }
+        return confidence;
+    }
+}
+
+export default Confidences;

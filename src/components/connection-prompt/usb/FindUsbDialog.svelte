1fe3105c13db86f810f560e5c3d3b5d69e86725e
r59q <andreashenriksen@yahoo.dk>
2024-09-17 21:50:56 +0200

    Fixed problematic uses of buttons

--- src/components/connection-prompt/usb/FindUsbDialog.svelte
+++ src/components/connection-prompt/usb/FindUsbDialog.svelte
@@ -43,13 +43,15 @@
         </p>
       {/if}
     </div>
-    <StandardButton
-      onClick={step === 2
-        ? onFindUsbClick
-        : () => {
-            step = 2;
-          }}>
-      {$t(step === 1 ? 'connectMB.usb.button1' : 'connectMB.usb.button2')}
-    </StandardButton>
+    <div class="flex justify-center">
+      <StandardButton
+        onClick={step === 2
+          ? onFindUsbClick
+          : () => {
+              step = 2;
+            }}>
+        {$t(step === 1 ? 'connectMB.usb.button1' : 'connectMB.usb.button2')}
+      </StandardButton>
+    </div>
   </div>
 </main>

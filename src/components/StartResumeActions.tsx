b907119b9200c1b0875cf49379be626e15029132
Grace <145345672+microbit-grace@users.noreply.github.com>
2024-09-18 14:19:32 +0100

    Navigate to data samples page when starting new session (#315)

--- src/components/StartResumeActions.tsx
+++ src/components/StartResumeActions.tsx
@@ -1,26 +1,19 @@
 import { Button, HStack, StackProps, useDisclosure } from "@chakra-ui/react";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { FormattedMessage } from "react-intl";
 import { useNavigate } from "react-router";
+import { useConnectionStage } from "../connection-stage-hooks";
+import { SessionPageId } from "../pages-config";
+import { useHasGestures, useStore } from "../store";
 import { createSessionPageUrl } from "../urls";
 import StartOverWarningDialog from "./StartOverWarningDialog";
-import { useConnectionStage } from "../connection-stage-hooks";
-import { ConnectionStatus } from "../connect-status-hooks";
-import { SessionPageId } from "../pages-config";
-import { useStore, useHasGestures } from "../store";
 
 const StartResumeActions = ({ ...props }: Partial<StackProps>) => {
   const newSession = useStore((s) => s.newSession);
   const hasExistingSession = useHasGestures();
-  const [hasConnectFlowStarted, setHasConnectFlowStarted] =
-    useState<boolean>(false);
   const startOverWarningDialogDisclosure = useDisclosure();
   const navigate = useNavigate();
-  const {
-    actions: connStageActions,
-    isConnected,
-    status,
-  } = useConnectionStage();
+  const { actions: connStageActions } = useConnectionStage();
 
   const handleNavigateToAddData = useCallback(() => {
     navigate(createSessionPageUrl(SessionPageId.DataSamples));
@@ -29,25 +22,14 @@ const StartResumeActions = ({ ...props }: Partial<StackProps>) => {
   const handleStartNewSession = useCallback(() => {
     startOverWarningDialogDisclosure.onClose();
     newSession();
-    if (isConnected) {
-      handleNavigateToAddData();
-    } else {
-      connStageActions.startConnect();
-      setHasConnectFlowStarted(true);
-    }
+    handleNavigateToAddData();
+    connStageActions.startConnect();
   }, [
     startOverWarningDialogDisclosure,
     newSession,
-    isConnected,
     handleNavigateToAddData,
     connStageActions,
   ]);
-
-  useEffect(() => {
-    if (status === ConnectionStatus.Connected && hasConnectFlowStarted) {
-      handleNavigateToAddData();
-    }
-  }, [handleNavigateToAddData, hasConnectFlowStarted, status]);
 
   const onClickStartNewSession = useCallback(() => {
     if (hasExistingSession) {

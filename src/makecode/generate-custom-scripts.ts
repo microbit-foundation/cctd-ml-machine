177afc3c8404be06a022c189de31fe7ae3aabbda
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2024-09-25 10:01:13 +0100

    Use npm release of header generator (#327)
    You should get a clean `npm i` with no config now we've moved it off
    GitHub Packages.

--- src/makecode/generate-custom-scripts.ts
+++ src/makecode/generate-custom-scripts.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  */
 import { compileModel } from "ml4f";
-import { generateBlob } from "@microbit-foundation/ml-header-generator";
+import { generateBlob } from "@microbit/ml-header-generator";
 import { ActionName, actionNamesFromLabels } from "./utils";
 import { LayersModel } from "@tensorflow/tfjs";
 import { mlSettings } from "../ml";

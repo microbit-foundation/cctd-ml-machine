36f3e50e73c4fd3a34d2d8ba590f3f69a98c3926
r59q <andreashenriksen@yahoo.dk>
2024-10-15 20:26:46 +0200

    Prettier

--- src/script/FeatureToggles.ts
+++ src/script/FeatureToggles.ts
@@ -12,7 +12,7 @@ export enum Feature {
   TITLE = 'title',
   LOSS_GRAPH = 'lossGraph',
   MAKECODE = 'makecode',
-  LIVE_GRAPH_INPUT_VALUES = 'liveGraphInputValues'
+  LIVE_GRAPH_INPUT_VALUES = 'liveGraphInputValues',
 }
 
 export const hasFeature = (feature: Feature): boolean => {

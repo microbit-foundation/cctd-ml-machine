b6963e44b72cbab7b6e306c0822d9cd6665b93e2
A. Malthe Henriksen <mail@r59q.com>
2025-02-10 20:37:46 +0100

    Hide Axes not highlighted

--- src/components/graphs/recording/RecordingGraphPointData.svelte
+++ src/components/graphs/recording/RecordingGraphPointData.svelte
@@ -4,16 +4,32 @@
   SPDX-License-Identifier: MIT
  -->
 <script lang="ts">
+  import { derived } from 'svelte/store';
+  import { stores } from '../../../script/stores/Stores';
   import StaticConfiguration from '../../../StaticConfiguration';
   import FixedNumber from '../../base/FixedNumber.svelte';
 
   export let sample: number[];
+  export let offest: number;
+  const highlightedAxes = stores.getHighlightedAxes();
+
+  const sampleEnabled = derived(highlightedAxes, axes => {
+    return sample.map((_, idx) => axes.find(axis => axis.index === idx) !== undefined);
+  });
+
+  $: noOfHighlightedAxes = $highlightedAxes.length;
 </script>
 
-<span>
-  {#each sample as sampleValue, idx}
-    <span class="mr-1" style="color: {StaticConfiguration.graphColors[idx]};">
-      <FixedNumber digits={2} number={sampleValue} />
-    </span>
-  {/each}
-</span>
+<p
+  style="left: calc({offest}px - {42 * noOfHighlightedAxes}px); top:-25px ;pointer-events:none"
+  class="absolute w-40">
+  <span>
+    {#each sample as sampleValue, idx}
+      {#if $sampleEnabled[idx]}
+        <span class="mr-1" style="color: {StaticConfiguration.graphColors[idx]};">
+          <FixedNumber digits={2} number={sampleValue} />
+        </span>
+      {/if}
+    {/each}
+  </span>
+</p>

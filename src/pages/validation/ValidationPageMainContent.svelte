6a53440c52d7a76542bf2c007c5a5c55baa59908
A. Malthe Henriksen <mail@r59q.com>
2025-04-15 22:58:48 +0200

    Add no gestures text

--- src/pages/validation/ValidationPageMainContent.svelte
+++ src/pages/validation/ValidationPageMainContent.svelte
@@ -19,25 +19,34 @@
   export let onNoMicrobitSelect: () => void;
 </script>
 
-<div class="p-3 gap-2 grid grid-cols-[max(200px,20%)_140px_1fr]">
-  <ValidationPageInformationLabels />
+{#if $gestures.length !== 0}
+  <div class="p-3 gap-2 grid grid-cols-[max(200px,20%)_140px_1fr]">
+    <ValidationPageInformationLabels />
 
-  {#each stores.getGestures().getGestures() as gesture, idx}
-    <div class="col-start-1">
-      <ValidationGestureNameCard gesture={gestures.getGesture(gesture.getId())} />
-    </div>
+    {#each stores.getGestures().getGestures() as gesture, idx}
+      <div class="col-start-1">
+        <ValidationGestureNameCard gesture={gestures.getGesture(gesture.getId())} />
+      </div>
 
-    <div class="col-start-2">
-      <ValidationPageRecordingIndicator gestureId={gesture.getId()} />
-      <ValidationGestureSelectGestureCard {gesture} {onNoMicrobitSelect} />
-    </div>
+      <div class="col-start-2">
+        <ValidationPageRecordingIndicator gestureId={gesture.getId()} />
+        <ValidationGestureSelectGestureCard {gesture} {onNoMicrobitSelect} />
+      </div>
 
-    <div class="col-start-3">
-      {#if !$isValidationSetEmpty}
-        <ValidationGestureRecordingsCard {gesture} />
-      {:else if $chosenGesture?.getId() === gesture.getId() || (!$chosenGesture && idx === 0)}
-        <ValidationPageTutorial />
-      {/if}
+      <div class="col-start-3">
+        {#if !$isValidationSetEmpty}
+          <ValidationGestureRecordingsCard {gesture} />
+        {:else if $chosenGesture?.getId() === gesture.getId() || (!$chosenGesture && idx === 0)}
+          <ValidationPageTutorial />
+        {/if}
+      </div>
+    {/each}
+  </div>
+{:else}
+  <div class="p-3 flex flex-col justify-center items-center min-h-full">
+    <div class="flex flex-col gap-4 max-w-400px">
+      <p class="text-lg font-bold">No gestures...</p>
+      <p class="">You haven't added any gestures yet. Go to the data step in the menu to the left and add some.</p>
     </div>
-  {/each}
-</div>
+  </div>
+{/if}

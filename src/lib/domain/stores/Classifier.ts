bb5d8f4beb957832e9d64e5a148acc6842c214c4
A. Malthe Henriksen <mail@r59q.com>
2025-06-25 22:47:10 +0200

    Add predicted filtered value to store

--- src/lib/domain/stores/Classifier.ts
+++ src/lib/domain/stores/Classifier.ts
@@ -7,8 +7,10 @@ import {
   type Readable,
   type Subscriber,
   type Unsubscriber,
+  type Writable,
   derived,
   get,
+  writable,
 } from 'svelte/store';
 import Filters from '../Filters';
 import Model, { type ModelData } from './Model';
@@ -19,15 +21,18 @@ import BaseVector from '../BaseVector';
 
 type ClassifierData = {
   model: ModelData;
+  filteredInput: BaseVector;
 };
 
 class Classifier implements Readable<ClassifierData> {
+  private filteredInput: Writable<BaseVector>;
   constructor(
     private model: Model,
     private filters: Filters,
     private gestures: Readable<Gesture[]>,
     private confidenceSetter: (gestureId: GestureID, confidence: number) => void,
   ) {
+    this.filteredInput = writable(new BaseVector([]));
     Logger.log('classifier', 'Initialized classifier');
   }
 
@@ -35,10 +40,12 @@ class Classifier implements Readable<ClassifierData> {
     run: Subscriber<ClassifierData>,
     invalidate?: ((value?: ClassifierData | undefined) => void) | undefined,
   ): Unsubscriber {
-    return derived([this.model], stores => {
+    return derived([this.model, this.filteredInput], stores => {
       const modelStore = stores[0];
+      const filteredInputStore = stores[1];
       return {
         model: modelStore,
+        filteredInput: filteredInputStore,
       };
     }).subscribe(run, invalidate);
   }
@@ -48,6 +55,7 @@ class Classifier implements Readable<ClassifierData> {
    */
   public async classify(input: ClassifierInput): Promise<void> {
     const filteredInput = new BaseVector(input.getInput(this.filters));
+    this.filteredInput.set(filteredInput);
     const predictions = await this.getModel().predict(filteredInput);
     predictions.forEach((confidence, index) => {
       const gesture = get(this.gestures)[index];

ed1253c9d2c9d9cf87fd10e855f4196d2668f337
A. Malthe Henriksen <mail@r59q.com>
2025-06-25 23:59:52 +0200

    Simplify logger

--- src/lib/utils/Logger.ts
+++ src/lib/utils/Logger.ts
@@ -11,7 +11,7 @@ import PersistantWritable from '../repository/PersistantWritable';
 const nsStore = new PersistantWritable(false, 'dev_ns');
 
 class Logger {
-  constructor(private origin: any) {}
+  constructor(private origin: any) { }
 
   public log(message: any, ...params: any[]) {
     Logger.log(this.origin, message, params);
@@ -55,11 +55,17 @@ export const welcomeLog = () => {
   ) {
     return;
   }
-  console.log(`⚙️ Development Mode :
-  Welcome to the ML-Machine development mode. 
-  To disable stacktrace in logs, ds() in console.
-  To Enable again stacktraces using es().
-  If you experience any bugs, please report them at https://github.com/microbit-foundation/cctd-ml-machine/issues`);
+  console.log(`⚙️ Development Mode:
+Welcome to the ML-Machine development environment. 
+You are currently running the application in development mode, which provides enhanced debugging capabilities.
+
+To disable stack traces in logs for a cleaner console output, use the 'ds()' command in the browser console.
+To re-enable stack traces for detailed debugging, use the 'es()' command.
+
+If you encounter any issues, unexpected behavior, or bugs, please report them to our team by opening an issue at:
+https://github.com/microbit-foundation/cctd-ml-machine/issues.
+
+Thank you for contributing to the improvement of ML-Machine!`);
   Object.assign(window, { hasLogged: true });
 };
 

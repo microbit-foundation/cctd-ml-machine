edfb24fb5f10ab06d96e3b433ad7de75010e6f5a
A. Malthe Henriksen <mail@r59q.com>
2025-09-09 22:23:56 +0200

    Add mkcd-ml-machine extension

--- src/lib/makecodeProject.ts
+++ src/lib/makecodeProject.ts
@@ -7,212 +7,22 @@ export const project: MakeCodeProject = {
     'README.md': ``,
 
     'main.blocks': `
-<xml xmlns="https://developers.google.com/blockly/xml">
-  <variables>
-    <variable id="5rH[22#dxH5~IM6x%PE+">timer</variable>
-    <variable id="gKD%iDyA}x49oH=]E{@O">item</variable>
-  </variables>
-  <block type="device_button_event" x="0" y="0">
-    <field name="NAME">Button.A</field>
-    <statement name="HANDLER">
-      <block type="variables_set">
-        <field name="VAR" id="5rH[22#dxH5~IM6x%PE+">timer</field>
-        <value name="VALUE">
-          <shadow type="math_number">
-            <field name="NUM">10</field>
-          </shadow>
-        </value>
-        <next>
-          <block type="device_while">
-            <value name="COND">
-              <shadow type="logic_boolean">
-                <field name="BOOL">TRUE</field>
-              </shadow>
-              <block type="logic_compare">
-                <field name="OP">GT</field>
-                <value name="A">
-                  <shadow type="math_number">
-                    <field name="NUM">0</field>
-                  </shadow>
-                  <block type="variables_get">
-                    <field name="VAR" id="5rH[22#dxH5~IM6x%PE+">timer</field>
-                  </block>
-                </value>
-                <value name="B">
-                  <shadow type="math_number">
-                    <field name="NUM">0</field>
-                  </shadow>
-                </value>
-              </block>
-            </value>
-            <statement name="DO">
-              <block type="automationbit_set_output">
-                <field name="output">automationbit.Output.Two</field>
-                <value name="state">
-                  <shadow type="math_number_minmax">
-                    <mutation min="0" max="1" label="State" precision="0"/>
-                    <field name="SLIDER">1</field>
-                  </shadow>
-                </value>
-                <next>
-                  <block type="automationbit_set_output">
-                    <field name="output">automationbit.Output.One</field>
-                    <value name="state">
-                      <shadow type="math_number_minmax">
-                        <mutation min="0" max="1" label="State" precision="0"/>
-                        <field name="SLIDER">1</field>
-                      </shadow>
-                    </value>
-                  </block>
-                </next>
-              </block>
-            </statement>
-            <next>
-              <block type="variables_set">
-                <field name="VAR" id="5rH[22#dxH5~IM6x%PE+">timer</field>
-                <value name="VALUE">
-                  <shadow type="math_number">
-                    <field name="NUM">2</field>
-                  </shadow>
-                </value>
-                <next>
-                  <block type="device_while">
-                    <value name="COND">
-                      <shadow type="logic_boolean">
-                        <field name="BOOL">TRUE</field>
-                      </shadow>
-                      <block type="logic_compare">
-                        <field name="OP">GT</field>
-                        <value name="A">
-                          <shadow type="math_number">
-                            <field name="NUM">0</field>
-                          </shadow>
-                          <block type="variables_get">
-                            <field name="VAR" id="5rH[22#dxH5~IM6x%PE+">timer</field>
-                          </block>
-                        </value>
-                        <value name="B">
-                          <shadow type="math_number">
-                            <field name="NUM">0</field>
-                          </shadow>
-                        </value>
-                      </block>
-                    </value>
-                    <statement name="DO">
-                      <block type="automationbit_set_output">
-                        <field name="output">automationbit.Output.One</field>
-                        <value name="state">
-                          <shadow type="math_number_minmax">
-                            <mutation min="0" max="1" label="State" precision="0"/>
-                            <field name="SLIDER">1</field>
-                          </shadow>
-                        </value>
-                        <next>
-                          <block type="automationbit_set_output">
-                            <field name="output">automationbit.Output.Two</field>
-                            <value name="state">
-                              <shadow type="math_number_minmax">
-                                <mutation min="0" max="1" label="State" precision="0"/>
-                                <field name="SLIDER">0</field>
-                              </shadow>
-                            </value>
-                          </block>
-                        </next>
-                      </block>
-                    </statement>
-                  </block>
-                </next>
-              </block>
-            </next>
-          </block>
-        </next>
-      </block>
-    </statement>
-  </block>
-  <block type="device_forever" x="358" y="1">
-    <statement name="HANDLER">
-      <block type="device_pause">
-        <value name="pause">
-          <shadow type="timePicker">
-            <field name="ms">1000</field>
-          </shadow>
-        </value>
-        <next>
-          <block type="variables_change">
-            <field name="VAR" id="5rH[22#dxH5~IM6x%PE+">timer</field>
-            <value name="VALUE">
-              <shadow type="math_number">
-                <field name="NUM">-1</field>
-              </shadow>
-            </value>
-          </block>
-        </next>
-      </block>
-    </statement>
-  </block>
-  <block type="device_button_event" x="359" y="208">
-    <field name="NAME">Button.B</field>
-    <statement name="HANDLER">
-      <block type="automationbit_set_output">
-        <field name="output">automationbit.Output.One</field>
-        <value name="state">
-          <shadow type="math_number_minmax">
-            <mutation min="0" max="1" label="State" precision="0"/>
-            <field name="SLIDER">0</field>
-          </shadow>
-        </value>
-        <next>
-          <block type="automationbit_set_output">
-            <field name="output">automationbit.Output.Two</field>
-            <value name="state">
-              <shadow type="math_number_minmax">
-                <mutation min="0" max="1" label="State" precision="0"/>
-                <field name="SLIDER">0</field>
-              </shadow>
-            </value>
-          </block>
-        </next>
-      </block>
-    </statement>
-  </block>
-</xml>
+<xml xmlns="https://developers.google.com/blockly/xml">/xml>
 `,
 
     'main.ts': `
-let timer = 0
-
-input.onButtonPressed(Button.B, function () {
-    automationbit.setOutput(automationbit.Output.One, 0)
-    automationbit.setOutput(automationbit.Output.Two, 0)
-})
-
-input.onButtonPressed(Button.A, function () {
-    timer = 10
-    while (timer > 0) {
-        automationbit.setOutput(automationbit.Output.Two, 1)
-        automationbit.setOutput(automationbit.Output.One, 1)
-    }
-    timer = 2
-    while (timer > 0) {
-        automationbit.setOutput(automationbit.Output.One, 1)
-        automationbit.setOutput(automationbit.Output.Two, 0)
-    }
-})
-
-basic.forever(function () {
-    basic.pause(1000)
-    timer += -1
+MLMachine.onGestureRecognized("idle", function () {
+    MLMachine.showPairingPattern()
 })
 `,
 
     'pxt.json': `
 {
-  "name": "OilSpillCleanerUpper",
+  "name": "MLMachineMakecode",
   "description": "",
   "dependencies": {
     "core": "*",
-    "radio": "*",
-    "automationbit": "github:pimoroni/pxt-automationbit#v0.0.2"
+    "mkcd-ml-machine": "github:r59q/mkcd-ml-machine#f3892a137bab7daeb6908fb05c8374fbaf88652a"
   },
   "files": [
     "main.blocks",
@@ -220,13 +30,13 @@ basic.forever(function () {
     "README.md"
   ],
   "targetVersions": {
-    "branch": "v1.2.13",
-    "tag": "v1.2.13",
-    "commits": "https://github.com/Microsoft/pxt-microbit/commits/5d5b348757b15c6d00f5b7f560fd69592ca29424",
-    "target": "1.2.13",
-    "pxt": "4.4.7"
+    "branch": "v6.0.28",
+    "tag": "v6.0.28",
+    "commits": "https://github.com/microsoft/pxt-microbit/commits/9d308fa3c282191768670a6558e4df8af2d715cf",
+    "target": "6.0.28",
+    "pxt": "9.0.19"
   },
-  "preferredEditor": "blocksprj"
+"preferredEditor": "tsprj"
 }
 `,
   },

0bc467bf7c3ce158666ab8770a17cc4180e7d20e
A. Malthe Henriksen <mail@r59q.com>
2026-01-13 20:18:50 +0100

    Add translations

--- src/pages/model/tileview/ModelPageTileView.svelte
+++ src/pages/model/tileview/ModelPageTileView.svelte
@@ -15,6 +15,7 @@
   import { getControllers } from '../../../backend/interface-adapter/MLMachine';
   import MakeCodeProjectBlocks from '../../../components/features/makecode/MakeCodeProjectBlocks.svelte';
   import { navigate, Paths } from '../../../router/Router';
+  import { t } from 'svelte-i18n';
 
   const devices = stores.getDevices();
   const classifier = stores.getClassifier();
@@ -103,18 +104,23 @@
         <div
           class="flex flex-row mt-12 mx-30 bg-backgroundlight border-secondary border-1 p-4 rounded justify-center shadow-xl">
           <div class="flex flex-col">
-            <p class="text-md font-bold text-primary text-center">MakeCode</p>
+            <p class="text-md font-bold text-primary text-center">
+              {$t('content.model.makecode.heading')}
+            </p>
             <p class="text-sm">
-              You can create a hex file on <a
-                on:click={openMakeCode}
-                class="text-secondary cursor-pointer">
-                MakeCode
-              </a>
+              {$t('content.model.makecode.description')}
+              <span on:click={openMakeCode} class="text-secondary cursor-pointer">
+                {$t('content.model.makecode.heading')}
+              </span>
             </p>
           </div>
         </div>
       {:else}
         <div class="mt-2 bg-backgroundlight rounded-md p-1 shadow-sm text-xs">
+          <div class="pb-2">
+            <span class="underline cursor-pointer text-sm" on:click={openMakeCode}
+              >{$t('content.model.makecode.edit')}</span>
+          </div>
           <MakeCodeProjectBlocks />
         </div>
       {/if}

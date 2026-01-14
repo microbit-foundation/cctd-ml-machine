0d0ae632908ac1ec98ff892c659df32d5b2e23ee
A. Malthe Henriksen <mail@r59q.com>
2026-01-14 21:36:35 +0100

    Extract logic to ts

---
+++ src/components/features/makecode/MakeCodeIntegration.ts
@@ -0,0 +1,64 @@
+/**
+ * (c) 2023-2026, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { createMakeCodeURL, MakeCodeFrameDriver } from '@microbit/makecode-embed';
+import Microbits from '../../../lib/microbit-interfacing/Microbits';
+import FileUtility from '../../../lib/repository/FileUtility';
+import { navigate, Paths } from '../../../router/Router';
+import { getControllers } from '../../../backend/interface-adapter/MLMachine';
+
+export const flashHexContent = async (hexContent: string) => {
+  try {
+    await Microbits.linkMicrobit();
+    Microbits.flashHexToLinked(console.log, hexContent);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const downloadHexContent = (hexContent: string, filename: string) => {
+  try {
+    FileUtility.downloadFile(hexContent, filename);
+  } catch (err) {
+    console.error('Failed to save .hex file', err);
+  }
+};
+
+export const createFrameDriver = (iframe: HTMLIFrameElement | undefined) => {
+  const controllers = getControllers();
+  const makeCodeController = controllers.getMakeCodeController();
+
+  const onBackButtonPressed = () => navigate(Paths.MODEL);
+  const driverRef = new MakeCodeFrameDriver(
+    {
+      onBack: onBackButtonPressed,
+      controllerId: 'MlMachine',
+      initialProjects: async () => [makeCodeController.getMakeCodeProject().get()],
+      // When the editor loads, hide the simulator to make more space
+      onEditorContentLoaded: e => driverRef.hideSimulator(),
+      onWorkspaceSave: e => makeCodeController.setMakeCodeProject(e.project),
+      onDownload: ({ hex }) => flashHexContent(hex),
+      onSave: ({ name, hex }) => downloadHexContent(hex, name),
+    },
+    () => iframe,
+  );
+  return driverRef;
+};
+export const createMakeCodeIframe = () => {
+  const iframe = document.createElement('iframe');
+  iframe.allow = 'usb; autoplay; camera; microphone;';
+  iframe.src = createMakeCodeURL(
+    'https://makecode.microbit.org',
+    undefined, // Version.
+    undefined, // Language.
+    2, // Controller.
+    undefined, // Query params.
+  );
+
+  iframe.width = '100%';
+  iframe.height = '100%';
+  return iframe;
+};
--- src/components/features/makecode/MakeCodeProjectView.svelte
+++ src/components/features/makecode/MakeCodeProjectView.svelte
@@ -5,19 +5,8 @@
  -->
 
 <script lang="ts">
-  import {
-    createMakeCodeURL,
-    MakeCodeFrameDriver,
-    type MakeCodeProject,
-  } from '@microbit/makecode-embed';
-  import FileUtility from '../../../lib/repository/FileUtility';
   import { onMount } from 'svelte';
-  import { navigate, Paths } from '../../../router/Router';
-  import { getControllers } from '../../../backend/interface-adapter/MLMachine';
-  import Microbits from '../../../lib/microbit-interfacing/Microbits';
-
-  const controllers = getControllers();
-  const makeCodeController = controllers.getMakeCodeController();
+  import { createFrameDriver, createMakeCodeIframe } from './MakeCodeIntegration';
 
   // const renderer = createMakeCodeRenderBlocks({});
   // renderer.initialize();
@@ -28,48 +17,9 @@
       return;
     }
 
-    const onBackButtonPressed = () => navigate(Paths.MODEL);
-
-    const iframe = document.createElement('iframe');
-    iframe.allow = 'usb; autoplay; camera; microphone;';
-    iframe.src = createMakeCodeURL(
-      'https://makecode.microbit.org',
-      undefined, // Version.
-      undefined, // Language.
-      2, // Controller.
-      undefined, // Query params.
-    );
-
-    iframe.width = '100%';
-    iframe.height = '100%';
-
+    const iframe = createMakeCodeIframe();
     // Create and initialise an instance of MakeCodeFrameDriver.
-    const driverRef = new MakeCodeFrameDriver(
-      {
-        onBack: onBackButtonPressed,
-        controllerId: 'MlMachine',
-        initialProjects: async () => [makeCodeController.getMakeCodeProject().get()],
-        // When the editor loads, hide the simulator to make more space
-        onEditorContentLoaded: e => driverRef.hideSimulator(),
-        onWorkspaceSave: e => makeCodeController.setMakeCodeProject(e.project),
-        onDownload: async e => {
-          try {
-            await Microbits.linkMicrobit();
-            Microbits.flashHexToLinked(console.log, e.hex);
-          } catch (error) {
-            console.log(error);
-          }
-        },
-        onSave: (file: { name: string; hex: string }) => {
-          try {
-            FileUtility.downloadFile(file.hex, file.name);
-          } catch (err) {
-            console.error('Failed to save .hex file', err);
-          }
-        },
-      },
-      () => iframe,
-    );
+    const driverRef = createFrameDriver(iframe);
     driverRef.initialize();
     driverRef.hideSimulator();
     frameElem.appendChild(iframe);

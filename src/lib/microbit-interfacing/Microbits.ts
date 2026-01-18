dc6eced0687e118a2d365388d9b8824222136bed
A. Malthe Henriksen <mail@r59q.com>
2026-01-18 14:01:43 +0100

    prettier

--- src/lib/microbit-interfacing/Microbits.ts
+++ src/lib/microbit-interfacing/Microbits.ts
@@ -327,7 +327,12 @@ class Microbits {
       const versionedPart = separated.find(part => {
         return MBSpecs.Utility.getModelNumberFromBoardID(part.boardId) === mbVersion;
       });
-      console.log('Separated universal hex parts:', separated, 'Using part:', versionedPart);
+      console.log(
+        'Separated universal hex parts:',
+        separated,
+        'Using part:',
+        versionedPart,
+      );
       if (!versionedPart) {
         throw new Error(
           `No compatible hex part found for micro:bit version ${mbVersion}`,

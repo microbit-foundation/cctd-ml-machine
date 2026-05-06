25ef75936ec3ac24f29afb622df54071b4a43eb6
A. Malthe Henriksen <mail@r59q.com>
2026-05-06 22:16:35 +0200

    Clean up dimension labels

--- src/components/features/graphs/DimensionLabels.svelte
+++ src/components/features/graphs/DimensionLabels.svelte
@@ -13,21 +13,9 @@
 
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte';
-  import { derived, type Unsubscriber } from 'svelte/store';
   import type SmoothedLiveData from '../../../lib/livedata/SmoothedLiveData';
-  import StaticConfiguration from '../../../StaticConfiguration';
   import type { LiveDataVector } from '../../../core/vector/LiveDataVector';
-  import { getControllers } from '../../../backend/interface-adapter/MLMachine';
-
-  type LabelData = {
-    id: number;
-    label: string;
-    color: string;
-    arrowHeight: number;
-    textHeight: number;
-  };
-  // The height of character is used to fix overlapping line labels
-  const CHARACTER_HEIGHT = 16;
+  import { DimensionLabelsController } from './DimensionLabels';
 
   export let liveData: SmoothedLiveData<LiveDataVector>;
   export let maxValue: number;
@@ -35,78 +23,28 @@
   export let graphHeight: number;
   export let hidden: boolean = false;
 
-  let unsubscribeFromLiveData: Unsubscriber | undefined = undefined;
+  const controller = new DimensionLabelsController(
+    liveData,
+    () => maxValue,
+    () => minValue,
+    () => graphHeight,
+  );
+
+  const labels = controller.labels;
+  const labelEnabled = controller.labelEnabled;
 
   onMount(() => {
-    unsubscribeFromLiveData = liveData.subscribe(data => {
-      const dataInArray = [];
-      for (const num of data.getValue()) {
-        dataInArray.push(num);
-      }
-      updateDimensionLabels(dataInArray);
-    });
+    controller.start();
   });
 
   onDestroy(() => {
-    unsubscribeFromLiveData?.();
+    controller.stop();
   });
-
-  // Initialize labels with all-0 values
-  const initializeLabels = (): LabelData[] => {
-    const labels = [];
-    for (let i = 0; i < liveData.getSeriesSize(); i++) {
-      const label = liveData.getLabels()[i];
-      const color = StaticConfiguration.graphColors[i];
-      labels.push({
-        label: label,
-        color: color,
-        arrowHeight: 0,
-        textHeight: 0,
-        id: i,
-      });
-    }
-    return labels;
-  };
-
-  let labels: LabelData[] = initializeLabels();
-
-  function updateDimensionLabels(axes: number[]) {
-    for (let i = 0; i < axes.length; i++) {
-      const normalMax = maxValue - minValue;
-      const normalValue = axes[labels[i].id] - minValue;
-      const newValue = (normalValue / normalMax) * graphHeight;
-      labels[i].arrowHeight = newValue + 4; // We add 4 to align the arrow to the graph line
-      // labelHeight will be overridden in fixOverlappingLabels if necessary
-      labels[i].textHeight = newValue - CHARACTER_HEIGHT + 2; // Subract height to align with arrow
-    }
-    fixOverlappingLabels();
-  }
-
-  function fixOverlappingLabels() {
-    const sortedLabels = labels.toSorted((a, b) => {
-      return a.arrowHeight - b.arrowHeight;
-    });
-    for (let i = 1; i < sortedLabels.length; i++) {
-      const element = sortedLabels[i];
-      const previousLabel = sortedLabels[i - 1];
-      if (element.textHeight < previousLabel.textHeight + CHARACTER_HEIGHT) {
-        // Label is overlapping, push it down.
-        element.textHeight = previousLabel.textHeight + CHARACTER_HEIGHT;
-      }
-    }
-  }
-
-  const highlightedAxes = getControllers().getAxisController().getSelectedAxes();
-  console.log($highlightedAxes);
-  const labelEnabled = derived(highlightedAxes, axes => {
-    return labels.map((_, idx) => axes.find(axis => axis.index === idx) !== undefined);
-  });
-  console.log('dimension labels isHidden? ' + hidden);
 </script>
 
 {#if !hidden}
   <div class="h-40 w-6 relative">
-    {#each labels as label, idx}
+    {#each $labels as label, idx}
       {#if $labelEnabled[idx]}
         <div
           class="absolute arrowLeft -m-3.5"

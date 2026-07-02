1f1a39fc042b9d540d1d077760e75f0cf0f40551
r59q <mail@r59q.com>
2026-07-02 22:27:12 +0200

    Prep test suite for fixes

--- src/__tests__/ml/model.test.ts
+++ src/__tests__/ml/model.test.ts
@@ -16,33 +16,33 @@ import { NeuralNetworkSettingsImpl } from '../../core/model/neural-network/Neura
 describe('ML Model tests', async () => {
   describe('Layers Model', async () => {
     test('Model should train the expected number of times', async () => {
-      // let iterations = 0;
-      // const trainingData = new TestTrainingDataRepository();
-      // const trainer = new NeuralNetworkModelTrainer(
-      //   new NeuralNetworkSettingsImpl(StaticConfiguration.defaultNeuralNetworkSettings),
-      // );
-      // const model = trainer.trainModel(trainingData);
-      // expect(iterations).toBe(
-      //   StaticConfiguration.defaultNeuralNetworkSettings.noOfEpochs,
-      // );
+      let iterations = 0;
+      const trainingData = new TestTrainingDataRepository();
+      const trainer = new NeuralNetworkModelTrainer(
+        new NeuralNetworkSettingsImpl(StaticConfiguration.defaultNeuralNetworkSettings),
+      );
+      const model = trainer.trainModel(trainingData);
+      expect(iterations).toBe(
+        StaticConfiguration.defaultNeuralNetworkSettings.noOfEpochs,
+      );
     });
   });
   describe('KNN-non normalized Model', async () => {
     test('Model should train the expected number of times', async () => {
-      // const trainingData = new TestTrainingDataRepository();
-      // const knnModel = await new KNNModelTrainer({
-      //   k: 2,
-      //   normalize: false,
-      //   numberOfClasses: 3
-      // }).trainModel(trainingData);
-      // const prediction1 = await knnModel.predict(
-      //   new BaseVector([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-      // );
-      // expect(prediction1.getValue()).toStrictEqual([0, 1, 0]);
-      // const prediction2 = await knnModel.predict(
-      //   new BaseVector([1, 1, 0, 0, 0, -2, 0, -3, 0]),
-      // );
-      // expect(prediction2.getValue()).toStrictEqual([0.5, 0, 0.5]);
+      const trainingData = new TestTrainingDataRepository();
+      const knnModel = await new KNNModelTrainer({
+        k: 2,
+        normalize: false,
+        numberOfClasses: 3
+      }).trainModel(trainingData);
+      const prediction1 = await knnModel.predict(
+        new BaseVector([0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      );
+      expect(prediction1.getValue()).toStrictEqual([0, 1, 0]);
+      const prediction2 = await knnModel.predict(
+        new BaseVector([1, 1, 0, 0, 0, -2, 0, -3, 0]),
+      );
+      expect(prediction2.getValue()).toStrictEqual([0.5, 0, 0.5]);
     });
   });
 });

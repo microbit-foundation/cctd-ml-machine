3a45cabcc43c9ab0bd380fb49df96fd5999fccdc
r59q <mail@r59q.com>
2026-07-08 13:02:09 +0200

    Fix missing identifier

--- src/backend/interface-listener/KNNModelPendingSettingsListener.ts
+++ src/backend/interface-listener/KNNModelPendingSettingsListener.ts
@@ -1,3 +1,9 @@
+/**
+ * (c) 2023-2026, Center for Computational Thinking and Design at Aarhus University and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 import { AccuracyClassifierEvaluator } from '../../core/classifier/evaluator/AccuracyClassifierEvaluator';
 import { VectorClassifier } from '../../core/classifier/vector-classifier/VectorClassifier';
 import ConsoleLogger from '../../core/logging/ConsoleLogger';
